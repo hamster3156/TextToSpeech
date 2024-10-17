69b7be39743027d451b1e82dda7dc82596cb1201
hamster <95272940+hamster3156@users.noreply.github.com>
2024-10-17 22:47:08 +0900

    Update ITextToSpeechPlayer.cs

--- Azure/TextToSpeech/ITextToSpeechPlayer.cs
+++ Azure/TextToSpeech/ITextToSpeechPlayer.cs
@@ -5,10 +5,23 @@ namespace Hamster.Azure.TextToSpeech
 {
     public interface ITextToSpeechPlayer
     {
+        /// <summary>
+        /// 読み上げ音声を変更する
+        /// </summary>
+        /// <param name="selectName">選択する名前</param>
         void ChangeReadingVoice(ReadingVoiceName selectName);
 
+        /// <summary>
+        /// 会話を再生する
+        /// </summary>
+        /// <param name="speakContext">会話内容</param>
+        /// <param name="ct"></param>
+        /// <returns></returns>
         UniTask PlaySpeakAsync(string speakContext, CancellationToken ct);
 
+        /// <summary>
+        /// 会話を止める
+        /// </summary>
         void StopSpeak();
     }
 }
